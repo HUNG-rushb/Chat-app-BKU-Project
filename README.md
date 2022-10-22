ab590082bb1a3e1f816380abfbf6a5a492f034c9
Trịnh Duy Hưng <57101685+HUNG-rushb@users.noreply.github.com>
2022-10-22 19:48:43 +0700

    ok

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Chat-app-BKU-Project
+
+https://oq6smgcs39.execute-api.ap-southeast-1.amazonaws.com/dev/
--- server/src/index.js
+++ server/src/index.js
@@ -1,31 +1,46 @@
 // Apollo
-const { ApolloServer } = require('apollo-server-lambda');
+const { ApolloServer } = require("apollo-server-lambda");
 const {
   ApolloServerPluginLandingPageLocalDefault,
-} = require('apollo-server-core');
+} = require("apollo-server-core");
 
 // Prisma
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 
 // Type definitions
-const typeDefs = require('./Type_Definitions/_typeDefs.js');
+// const typeDefs = require('./Type_Definitions/_typeDefs.js');
 
 // Resolvers
-const Query = require('./resolvers/Query/_Query.js');
-const Mutation = require('./resolvers/Mutation/_Mutation.js');
-const Subcription = require('./resolvers/Subcription/_Subcription.js');
-const Type = require('./resolvers/Type/_Type.js');
+const Query = require("./resolvers/Query/_Query.js");
+const Mutation = require("./resolvers/Mutation/_Mutation.js");
+const Subcription = require("./resolvers/Subcription/_Subcription.js");
+const Type = require("./resolvers/Type/_Type.js");
 
 // Connect to MongoDB
 const prisma = new PrismaClient();
 exports.prisma;
 
-// Resolvers
+// // Resolvers
+// const resolvers = {
+//   Query,
+//   Mutation,
+//   // Subcription,
+//   ...Type,
+// };
+
+// Delete Later
+const { gql } = require("apollo-server-lambda");
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+// Provide resolver functions for your schema fields
 const resolvers = {
-  Query,
-  Mutation,
-  // Subcription,
-  ...Type,
+  Query: {
+    hello: () => "Hello world!",
+  },
 };
 
 // The server
@@ -34,7 +49,7 @@ const server = new ApolloServer({
   resolvers,
   context: { prisma },
   csrfPrevention: true,
-  cache: 'bounded',
+  cache: "bounded",
   plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
 });
 
